c7b36d67d13f5c1509e3fd7a259c2b023b8f66e5
merlinmiao <820962493@qq.com>
2017-07-17 16:31:30 +0800

    2017-07-17
    2017-07-17

---
+++ Vue/start-vue/main.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: '页面加载于 ' + new Date(),
+    question:'question'
+  }
+})
+
+Vue.component('button-counter', {
+  template: '<button v-on:click="increment">{{ counter }}</button>',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment: function () {
+      this.counter += 1
+      this.$emit('increment')
+    }
+  },
+})
+var app3=new Vue({
+  el: '#counter-event-example',
+  data: {
+    total: 0
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total += 1
+    }
+  }
+})
